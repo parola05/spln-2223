504abf2e65adb7a2c13d8c5902c6bfc6f160879a
henrique parola <parolajc@hotmail.com>
2023-03-21 13:55:46 +0000

    refactor: refactor project structure

--- TPC4/hpln/hpln.py
+++ TPC4/hpln/hpln.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
+"""Processador de linguagem natural!"""
+import sys 
+import fileinput 
+import argparse
+import sys
 import re
+
+__version__ = "0.1"
 
 '''
     Remove '\n' from the input text
@@ -65,3 +73,44 @@ def getContentByPage(text,page):
 def getWords(text):
     words = re.split(r"\s", text)
     return words
+
+parser = argparse.ArgumentParser(description='Processador de linguagem natural')
+parser.add_argument("--fl", action="store_true", help="Retorna uma frase por linha")
+parser.add_argument("--pl", action="store_true", help="Retorna uma palavra por linha")
+parser.add_argument("--pag", type=int, help="Retorna o conteúdo da página fornecida")
+parser.add_argument('texto', type=argparse.FileType('r'), help='Arquivo de texto de entrada')
+
+args = parser.parse_args()
+
+text = ''
+text = args.texto.read()
+
+def main():
+    global text
+     
+    if args.fl: # Frases por Linha
+        text = removeNewLine(text)
+        text = changeElipses(text)
+        text = changeSrAndSrta(text)
+        sentences = getSentences(text)
+
+        for i in range(0,len(sentences)):
+            sentences[i] = recoverElipses(sentences[i])
+            sentences[i] = recoverSrAndSraAndSrta(sentences[i])
+
+        for sentence in sentences:
+            print(sentence)
+    elif args.pl: # Palavra por Linha
+        words = getWords(text)
+
+        for word in words:
+            print(word)   
+    elif args.pag: # Conteúdo da uma página
+        content = getContentByPage(text,args.pag)
+        print("Page " + str(args.pag) + ":\n")
+        print(content)
+    else:
+        pass
+
+if __name__ == "__main__":
+    main()
