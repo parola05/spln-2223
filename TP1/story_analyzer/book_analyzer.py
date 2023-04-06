2bd9c280c3b30dece1d26e583f6a21f61e5f7c2d
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2023-04-06 16:24:25 +0100

    add aqueryActions and querySentences

--- TP1/story_analyzer/book_analyzer.py
+++ TP1/story_analyzer/book_analyzer.py
@@ -1,5 +1,6 @@
 import spacy
 import os
+from collections import Counter
 from typing import List
 
 class ParserModels:
@@ -19,11 +20,29 @@ class ParserModels:
 
 
 class BookAnalyzer:
-    def __init__(self, language : str):
+    def __init__(self, language : str, text : str):
         self.parser: ParserModels = ParserModels()
         self.model :str = self.__modelFromLanguage(language)
         self.nlp = spacy.load(self.model)
+        self.doc = self.nlp(text)
+        self.maxElements = 100
 
     def __modelFromLanguage(self, language: str) -> str :
         return self.parser.getModel(language)
+
+    def queryActions(self):
+        "Gets the most actions of a book"
+        verbs: Counter = Counter()
+        for w in self.doc:
+            if w.pos_ == "VERB":
+                verbs[w.lemma_] += 1
+        return verbs.most_common(self.maxElements)
+
+    def querySentences(self):
+        "Gets the number of sentences of a text"
+        return len(self.doc.sents)
+
+
+
+
 
