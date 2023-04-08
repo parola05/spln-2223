234a8a7ad9db4d22326e8fa701d1d3ac6d2df09b
franl08 <franeves08@gmail.com>
2023-04-08 16:53:03 +0100

    Finished parsing HTML book, added colors to panic function

--- TP1/story_analyzer/get_book.py
+++ TP1/story_analyzer/get_book.py
@@ -1,84 +1,84 @@
 import requests
+import re
 from bs4 import BeautifulSoup, NavigableString
 
-# TODO: need to check conditions, maybe use the other format
-# Check if it is only accumulate <p></p> tags
+
+def decompose_tag(tags):
+    for tag in tags:
+        tag.decompose()
+
+
+def remove_useless_tags(soup):
+    decompose_tag(soup.find_all('head'))
+    decompose_tag(soup.find_all('div', {'class': 'footnote'}))
+    decompose_tag(soup.find_all('div', {'class': 'footnotes'}))
+    decompose_tag(soup.find_all('hr'))
+    decompose_tag(soup.find_all('span', {'class': 'pagenum'}))
+    decompose_tag(soup.find_all('div', {'class': 'caption'}))
+    decompose_tag(soup.find_all('span', {'class': 'caption'}))
+    decompose_tag(soup.find_all('div', {'class': 'toc'}))
+    decompose_tag(soup.find_all('p', {'class': 'toc'}))
+    decompose_tag(soup.find_all('div', {'class': 'blk'}))
+    decompose_tag(soup.find_all('h1'))
+    decompose_tag(soup.find_all('section'))
+    decompose_tag(soup.find_all('div', {'class': 'dedication'}))
+    decompose_tag(soup.find_all('div', {'class': 'epigraph'}))
+    decompose_tag(soup.find_all('h2'))
+    decompose_tag(soup.find_all('div', {'class': 'figcenter'}))
+    decompose_tag(soup.find_all('a'))
+    decompose_tag(soup.find_all('img'))
+    decompose_tag(soup.find_all('p', {'class': 'c'}))
+    decompose_tag(soup.find_all('table'))
+    decompose_tag(soup.find_all('div', style=True))
+    return soup
+
 
 def treat_html(html):
-    text = ""
-    soup = BeautifulSoup(html, 'html.parser')
-    pages = soup.find_all('span', {'class': 'pagenum'})
-    captions = soup.find_all('div', {'class': 'caption'})
-    span_captions = soup.find_all('span', {'class': 'caption'})
-    tocs = soup.find_all('p', {'class': 'toc'})
-    blks = soup.find_all('div', {'class': 'blk'})
-    h2s = soup.find_all('h2')
-    figcenters = soup.find_all('div', {'class': 'figcenter'})
-    anchors = soup.find_all('a')
-    images = soup.find_all('img')
-    pcs = soup.find_all('p', {'class': 'c'})
-    tables = soup.find_all('table')
-    for page in pages:
-        page.extract()
-    for caption in captions:
-        caption.extract()
-    for h2 in h2s:
-        h2.extract()
-    for span_caption in span_captions:
-        span_caption.extract()
-    for figcenter in figcenters:
-        figcenter.extract()
-    for anchor in anchors:
-        anchor.extract()
-    for blk in blks:
-        blk.extract()
-    for toc in tocs:
-        toc.extract()
-    for pc in pcs:
-        pc.extract()
-    for image in images:
-        image.extract()
-    for table in tables:
-        table.extract()
-    hr = soup.find('hr', {'class': 'full'})
+    result = ""
+    soup = remove_useless_tags(BeautifulSoup(html, 'html.parser'))
     nested = []
-    for tag in hr.find_all_next():
+    for tag in soup.find_all():
         if tag in nested:
             nested.remove(tag)
-        elif tag.name == "hr":
-            break
         elif isinstance(tag, NavigableString):
-            text += tag
+            result += tag
         else:
             nested_tags = [
                 t for t in tag.descendants if t.name is not None]
             for x in nested_tags:
                 nested.append(x)
             if tag not in nested:
-                text += tag.get_text()
-    return text
+                result += tag.get_text()
+    return result
 
-def get_book(mode,book_name):
+
+def trim_newlines(text):
+    return re.sub(r'\s*\n\s*', '\n', text)
+
+
+def get_book(mode, book_name):
     book_string = ''
-    
+
     if mode == 'local':
         input_file = open(book_name, 'r')
         book_string = input_file.read()
-        input_file.close
+        input_file.close()
     elif mode == 'web':
         search_url = f"http://www.gutenberg.org/ebooks/search/?query={book_name}"
         response = requests.get(search_url)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            book_id = soup.find('li', {'class': 'booklink'}).find('a')['href'].split('/')[-1]
-            book_html_url = f"http://www.gutenberg.org/files/{book_id}/{book_id}-h/{book_id}-h.htm"
+            book_id = soup.find('li', {'class': 'booklink'}).find('a')[
+                'href'].split('/')[-1]
+            book_html_url = f"http://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}-images.html"
             response = requests.get(book_html_url)
             if response.status_code == 200:
-                book_string = treat_html(response.content)
+                book_string = trim_newlines(treat_html(response.content))
+                print(book_string)
             else:
                 raise ValueError(f"unavailable html version of the book")
         else:
             raise ValueError(f"unavailable book: {book_name}")
-    
+
     return book_string
--- TP1/story_analyzer/main.py
+++ TP1/story_analyzer/main.py
@@ -2,30 +2,48 @@ import argparse
 import get_book
 from book import Book
 import re
+import colorama
+from colorama import Fore, Style
+
 
 def panic(message):
-    print(f"error: {message}")  # todo: colors / warnings vs errors
-    exit(1)  
+    print(Fore.RED + f"error: {message}")
+    reset = Style.RESET_ALL
+    exit(1)
+
 
 def main():
-    args_parser = argparse.ArgumentParser(description='Book Analyzer: get insight informations of your storie')
-    args_parser.add_argument('input', nargs=1, type=str, metavar='input', help='input file path or book name (only in web mode)')
-    args_parser.add_argument('output', nargs=1, type=str,metavar='output', help='output file path')
-    args_parser.add_argument('-m', '--mode', nargs=1, type=str, choices=['local', 'web'], help='app modes', default=['local'])
-    args_parser.add_argument('-q', '--quiz', action='store_true', help='quiz game')
-    args_parser.add_argument('-t', '--translate', nargs='?', type=str, choices=['English', 'Spanish', 'French', 'German', 'Italian', 'Portuguese'], help='translate the book', const=['English'])
-    args_parser.add_argument('-d', '--discussions', action='store_true', help='list the book discussions (topics)')
-    args_parser.add_argument('-s', '--summary', action='store_true', help='summarize the book')
-    args_parser.add_argument('-l', '--language', action='store_true', help='detect book language')
-    args_parser.add_argument('-c', '--characters', action='store_true', help='get informations of the book characters')
-    args_parser.add_argument('-a', '--actions', nargs='?', type=int, help='get the top most actions of a book', const=10)
-    args_parser.add_argument('--save', nargs=1, type=str, help='the book will be saved and so will any queries invoked deemed savable.', metavar='title')
-    args_parser.add_argument('-p', '--projection', nargs=1, type=str, help='project queries in the text range. Projetion type is [<bottom>;<higher>]')
+    args_parser = argparse.ArgumentParser(
+        description='Book Analyzer: get insight informations of your storie')
+    args_parser.add_argument('input', nargs=1, type=str, metavar='input',
+                             help='input file path or book name (only in web mode)')
+    args_parser.add_argument('output', nargs=1, type=str,
+                             metavar='output', help='output file path')
+    args_parser.add_argument('-m', '--mode', nargs=1, type=str,
+                             choices=['local', 'web'], help='app modes', default=['local'])
+    args_parser.add_argument(
+        '-q', '--quiz', action='store_true', help='quiz game')
+    args_parser.add_argument('-t', '--translate', nargs='?', type=str, choices=[
+                             'English', 'Spanish', 'French', 'German', 'Italian', 'Portuguese'], help='translate the book', const=['English'])
+    args_parser.add_argument(
+        '-d', '--discussions', action='store_true', help='list the book discussions (topics)')
+    args_parser.add_argument(
+        '-s', '--summary', action='store_true', help='summarize the book')
+    args_parser.add_argument(
+        '-l', '--language', action='store_true', help='detect book language')
+    args_parser.add_argument('-c', '--characters', action='store_true',
+                             help='get informations of the book characters')
+    args_parser.add_argument('-a', '--actions', nargs='?', type=int,
+                             help='get the top most actions of a book', const=10)
+    args_parser.add_argument('--save', nargs=1, type=str,
+                             help='the book will be saved and so will any queries invoked deemed savable.', metavar='title')
+    args_parser.add_argument('-p', '--projection', nargs=1, type=str,
+                             help='project queries in the text range. Projetion type is [<bottom>;<higher>]')
 
     args = args_parser.parse_args()
 
     try:
-        book_content = get_book.get_book(args.mode[0],args.input[0])
+        book_content = get_book.get_book(args.mode[0], args.input[0])
 
         # limit book_content with projection is used
         if args.projection:
@@ -39,11 +57,11 @@ def main():
                 bottom_limit = int((bottom_limit * len(book_content)) / 100)
                 higher_limit = int((higher_limit * len(book_content)) / 100)
                 book_content = book_content[bottom_limit:higher_limit]
-        
+
         book = Book(book_content)
 
         if args.actions:
-           print(book.spacy_queries.queryActions(args.actions))
+            print(book.spacy_queries.queryActions(args.actions))
         elif args.language:
             print(book.queryLanguage())
         elif args.quiz:
@@ -51,7 +69,7 @@ def main():
             quiz_sentences = book.quiz()
 
             for i, phrase in enumerate(quiz_sentences):
-                print("[",i,"]",str(phrase),"\n")
+                print("[", i, "]", str(phrase), "\n")
         elif args.translate:
             print(book.translate(args.translate))
         elif args.summary:
@@ -64,6 +82,7 @@ def main():
 
     except ValueError as e:
         panic(str(e))
+
 
 if __name__ == "__main__":
     main()
