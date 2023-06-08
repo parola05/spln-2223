4d79ec77c3429a7d533076c97e69e64ccf89c927
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2023-06-08 12:32:31 +0100

    feat: add init_window and add test for document_shield

--- TP2/hshield/document_shield.py
+++ TP2/hshield/document_shield.py
@@ -1,6 +1,26 @@
+import spacy
+import re
+
 class DocumentShield():
     def __init__(self,text) -> None:
         self.text = text
+        self.nlp = spacy.load("pt_core_news_md")
+        self.doc = self.nlp(self.text)
+        self.window = self.init_window(15, text)
+        self.window_text = "".join(self.window)
+        self.window_pos = 0
+        self.window_pos_max = len(self.doc)
+
+    def init_window(self, size, text):
+        window = []
+        for (i, token) in enumerate(self.doc):
+            window.append(token.lemma_ + " ")
+            if i == size:
+                break
+        return window
+
+    def slide_right(self):
+        pass
 
     def shield(self):
         '''
---
+++ TP2/hshield/tests/doc_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from hshield.document_shield import DocumentShield
+text = "Número de carta de condução: BR-123456 1 agora vou ligar à carla o número dela " \
+       "é 912345678! Já agora o meu número de cc é 11340443"
+
+test = DocumentShield(text)
+print(test.window_text)
