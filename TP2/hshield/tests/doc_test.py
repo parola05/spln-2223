e72710b00e0595094d8ee17099618121b8077154
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2023-06-08 22:06:44 +0100

    fix: add extra 0 in cc

--- TP2/hshield/tests/doc_test.py
+++ TP2/hshield/tests/doc_test.py
@@ -2,7 +2,7 @@
 import hshield.document_shield
 from hshield.document_shield import DocumentShield
 text = "Número de carta de condução: BR-123456 1 agora vou ligar à carla o número dela " \
-       "é 912345678! Já agora o meu número de cc é 0000000 0 ZZ4"
+       "é 912345678! Já agora o meu número de cc é 00000000 0 ZZ4"
 
 test = DocumentShield(text)
 
